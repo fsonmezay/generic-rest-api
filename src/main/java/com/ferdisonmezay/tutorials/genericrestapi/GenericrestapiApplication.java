e3244e18a67aa6cb2d9945902157eb6d0a44ee22
Ferdi Sonmezay <ferdi@fsdebian.ferdisonmezay.com>
2017-07-17 10:30:52 +0300

    initial project structure

---
+++ src/main/java/com/ferdisonmezay/tutorials/genericrestapi/GenericrestapiApplication.java
@@ -0,0 +1,12 @@
+package com.ferdisonmezay.tutorials.genericrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericrestapiApplication.class, args);
+	}
+}
